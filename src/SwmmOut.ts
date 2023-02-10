70a8bc4cfa02f44627476a3497a067edb21856bd
swmm-js <88747437+swmm-js@users.noreply.github.com>
2023-02-10 08:21:44 -0800

    Updates to read functions, comments.

--- src/SwmmOut.ts
+++ src/SwmmOut.ts
@@ -1,45 +1,57 @@
 export class SwmmOut {
   value: ArrayBuffer
-  RECORDSIZE: number
+  // Because RECORDSIZE is not variable and may need 
+  // to be requested without initializing a SwmmOut object, it is static. 
+  static RECORDSIZE: number = 4
 
   constructor(n: ArrayBuffer) {
     this.value = n
-    this.RECORDSIZE = 4
   }
 
+  /**
+  * Returns the object's core value.
+  *
+  * @return {ArrayBuffer} Binary contents of a .out or simulated .out file.
+  */
   val(): ArrayBuffer {
     return this.value
   }
 
+  /**
+  * Returns the object's EPA-SWMM version number.
+  *
+  * @return {number} Integer. EPA-SWMM version number.
+  */
   version(): number{
-    return this.readInt(this.RECORDSIZE)
+    return this.readInt(SwmmOut.RECORDSIZE)
   }
 
+  /**
+  * Returns the object's first magic number.
+  *
+  * @return {number} Integer. Flow unit code.
+  */
   magic1(): number{
     return this.readInt(0)
   }
 
   /**
-  * Returns x raised to the n-th power.
+  * Returns the flow unit code.
   *
-  * @param {number} x The number to raise.
-  * @param {number} n The power, must be a natural number.
-  * @return {number} x raised to the n-th power.
+  * @return {number} Integer. Flow unit code.
   */
   flowUnitCode(): number{
-    return this.readInt(this.RECORDSIZE * 2)
+    return this.readInt(SwmmOut.RECORDSIZE * 2)
   }
 
-  // When reading an integer value from a binary file.
+  /**
+  * Reads a 32-bit signed integer from a position in SwmmOut.
+  *
+  * @param {number} offset The position of the readable integer, in number of bytes from the start of the SwmmOut object.
+  * @return {number} An integer read from SwmmOut.
+  */
   readInt(offset:number) {
-    return new Int32Array(this.value.slice(offset, offset + this.RECORDSIZE))[0]
-  }
-/*
-  toString(): string {
-    return this.val().toString()
+    return new Int32Array(this.value.slice(offset, offset + SwmmOut.RECORDSIZE))[0]
   }
 
-  static addAll(numArr: Array<Num>): Num {
-    return new Num(numArr.map((n) => n.val()).reduce((a, b) => a + b, 0))
-  }*/
 }
