a5212d5d79ca2b47908186df504c4e538243980e
swmm-js <88747437+swmm-js@users.noreply.github.com>
2023-02-10 10:04:26 -0800

    More jest test refactoring

--- test/SwmmOut.test.ts
+++ test/SwmmOut.test.ts
@@ -12,34 +12,22 @@ beforeAll(async () => {
   (global as any).example1 = new SwmmOut(file.buffer)
 })
 
+////////////////////////////////////////////////////////////////////////////////////////
+// OPENING RECORDS
+////////////////////////////////////////////////////////////////////////////////////////
+
 test('version_alt', () =>{
   expect((global as any).example1.version()).toBe(52002)
 })
 
 test('magic1', () =>{
-  // Read a .out file and initialize swmmOut with it
-  let outFile = './test/data/Example1.out'
-  fs.readFile(outFile, (err:any, data:any) => {
-    if(err) throw err
-    expect(new SwmmOut(data.buffer).magic1()).toBe(516114522)
-  })
+  expect((global as any).example1.magic1()).toBe(516114522)
 })
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // CLOSING RECORDS
 ////////////////////////////////////////////////////////////////////////////////////////
   
-  /**
-  * Returns the memory position of the Object ID names, 
-  * relative to the start of the SwmmOut object.
-  *
-  * @return {number} Integer. byte position of Object ID names.
-  */
-  test('ObjectIDNames', () =>{
-    // Read a .out file and initialize swmmOut with it
-    let outFile = './test/data/Example1.out'
-    fs.readFile(outFile, (err:any, data:any) => {
-      if(err) throw err
-      expect(new SwmmOut(data.buffer).ObjectIDNames()).toBe(28)
-    })
-  })
+test('ObjectIDNames', () =>{
+  expect((global as any).example1.ObjectIDNames()).toBe(28)
+})
