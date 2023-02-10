6c600408e995acc2ec2148c75cc700191e231c51
swmm-js <88747437+swmm-js@users.noreply.github.com>
2023-02-10 10:00:26 -0800

    Code refactor on jest file opening

--- test/SwmmOut.test.ts
+++ test/SwmmOut.test.ts
@@ -1,14 +1,19 @@
 // SwmmOut.tests.ts
 import { SwmmOut } from '../src/index'
-const fs = require('fs')
+import util from 'util'
+import fs from 'fs'
 
-test('version', () =>{
-  // Read a .out file and initialize swmmOut with it
-  let outFile = './test/data/Example1.out'
-  fs.readFile(outFile, (err:any, data:any) => {
-    if(err) throw err
-    expect(new SwmmOut(data.buffer).version()).toBe(52002)
-  })
+let test_Example1 = './test/data/Example1.out'
+
+// Prior to running tests, open files and set objects.
+beforeAll(async () => {
+  const readFile = util.promisify(fs.readFile)
+  const file = await readFile(test_Example1);
+  (global as any).example1 = new SwmmOut(file.buffer)
+})
+
+test('version_alt', () =>{
+  expect((global as any).example1.version()).toBe(52002)
 })
 
 test('magic1', () =>{
@@ -35,6 +40,6 @@ test('magic1', () =>{
     let outFile = './test/data/Example1.out'
     fs.readFile(outFile, (err:any, data:any) => {
       if(err) throw err
-      expect(new SwmmOut(data.buffer).ObjectIDNames()).toBe(516114522)
+      expect(new SwmmOut(data.buffer).ObjectIDNames()).toBe(28)
     })
   })
